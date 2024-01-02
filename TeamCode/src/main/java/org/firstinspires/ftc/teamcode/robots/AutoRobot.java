a4c2f2377d2d25cea8ef96885681e8bc0b048376
Oliver Rapp <oarapp3@gmail.com>
2024-01-01 19:44:21 -0500

    Deleted old files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/AutoRobot.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.robots;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-public class AutoRobot extends Robot {
-    public AutoRobot(OpMode opmode) {
-        super(opmode);
-    }
-}
