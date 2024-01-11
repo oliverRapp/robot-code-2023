b42eb1a0051fc7042def3fabd5ed119602506e30
Oliver Rapp <oarapp3@gmail.com>
2024-01-11 15:33:16 -0500

    Added drone system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.robot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Drone;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Gripper;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Slide;
 import org.firstinspires.ftc.teamcode.robot.subsystems.SlidePivot;
@@ -20,6 +21,7 @@ public class Robot {
     public Slide slide;
     public Wrist wrist;
     public Gripper gripper;
+    public Drone drone;
 
     public Robot(HardwareMap hwMap) {
         drivetrain = new Drivetrain(hwMap);
@@ -27,6 +29,7 @@ public class Robot {
         slide = new Slide(hwMap);
         wrist = new Wrist(hwMap);
         gripper = new Gripper(hwMap);
+        drone = new Drone(hwMap);
     }
 
     public void moveToPosition(Positions pos) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drone.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Drone {
+    private final double DRONE_CLOSE_POS = 0;
+    private final double DRONE_RELEASE_POS = 1;
+
+    private Servo droneServo;
+
+    public Drone(HardwareMap hwMap) {
+        droneServo = hwMap.get(Servo.class, "drone");
+    }
+
+    public void close() {
+        droneServo.setPosition(DRONE_CLOSE_POS);
+    }
+
+    public void release() {
+        droneServo.setPosition(DRONE_RELEASE_POS);
+    }
+}
