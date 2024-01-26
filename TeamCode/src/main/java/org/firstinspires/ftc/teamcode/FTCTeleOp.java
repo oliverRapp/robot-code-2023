61338ca5a8a2024da0e43e5d41029686b17a3334
Oliver Rapp <oarapp3@gmail.com>
2024-01-26 00:10:16 -0500

    Made Gamepad variables local

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCTeleOp.java
@@ -5,31 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
-@TeleOp(name = "Main TeleOp (Gamepad Test)")
+@TeleOp(name = "Main TeleOp")
 public class FTCTeleOp extends OpMode {
-    private double GAMEPAD_DRIVE;
-    private double GAMEPAD_TURN;
 
-    private boolean GAMEPAD_RAISE_PIVOT;
-    private boolean GAMEPAD_LOWER_PIVOT;
-
-    private float GAMEPAD_EXTEND_SLIDE;
-    private float GAMEPAD_RETRACT_SLIDE;
-
-    private boolean GAMEPAD_RAISE_WRIST;
-    private boolean GAMEPAD_LOWER_WRIST;
-
-    private boolean GAMEPAD_OPEN_GRIPPER;
-    private boolean GAMEPAD_CLOSE_GRIPPER;
-
-    // Add drone button
-    private boolean GAMEPAD_RELEASE_DRONE;
-
-    private boolean GAMEPAD_COLLECTION;
-    private boolean GAMEPAD_SECURE;
-    private boolean GAMEPAD_DEPOSITION;
-
-    Robot robot = null;
+    private Robot robot = null;
 
     @Override
     public void init() {
@@ -50,27 +29,28 @@ public class FTCTeleOp extends OpMode {
 
     @Override
     public void loop() {
-        GAMEPAD_DRIVE = gamepad1.left_stick_y;
-        GAMEPAD_TURN = gamepad1.right_stick_x;
+        double GAMEPAD_DRIVE = gamepad1.left_stick_y;
+        double GAMEPAD_TURN = gamepad1.right_stick_x;
 
-        GAMEPAD_RAISE_PIVOT = gamepad1.right_bumper;
-        GAMEPAD_LOWER_PIVOT = gamepad1.left_bumper;
+        boolean GAMEPAD_RAISE_PIVOT = gamepad1.right_bumper;
+        boolean GAMEPAD_LOWER_PIVOT = gamepad1.left_bumper;
 
-        GAMEPAD_EXTEND_SLIDE = gamepad1.right_trigger;
-        GAMEPAD_RETRACT_SLIDE = gamepad1.left_trigger;
+        float GAMEPAD_EXTEND_SLIDE = gamepad1.right_trigger;
+        float GAMEPAD_RETRACT_SLIDE = gamepad1.left_trigger;
 
-        GAMEPAD_RAISE_WRIST = gamepad1.dpad_up;
-        GAMEPAD_LOWER_WRIST = gamepad1.dpad_down;
+        boolean GAMEPAD_RAISE_WRIST = gamepad1.dpad_up;
+        boolean GAMEPAD_LOWER_WRIST = gamepad1.dpad_down;
 
-        GAMEPAD_OPEN_GRIPPER = gamepad1.dpad_right;
-        GAMEPAD_CLOSE_GRIPPER = gamepad1.dpad_left;
+        boolean GAMEPAD_OPEN_GRIPPER = gamepad1.dpad_right;
+        boolean GAMEPAD_CLOSE_GRIPPER = gamepad1.dpad_left;
 
         // Add drone button
-        GAMEPAD_RELEASE_DRONE = gamepad1.b;
+        // Add drone button
+        boolean GAMEPAD_RELEASE_DRONE = gamepad1.b;
 
-        GAMEPAD_COLLECTION = gamepad1.a;
-        GAMEPAD_SECURE = gamepad1.x;
-        GAMEPAD_DEPOSITION = gamepad1.y;
+        boolean GAMEPAD_COLLECTION = gamepad1.a;
+        boolean GAMEPAD_SECURE = gamepad1.x;
+        boolean GAMEPAD_DEPOSITION = gamepad1.y;
 
 
         // Driving
